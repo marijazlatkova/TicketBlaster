9077d58432962afb4c3b9c8714ae91110dbeb630
Marija Zlatkova <marijazlatkova44@yahoo.com>
2024-03-27 16:44:02 +0100

    users css

--- TicketBlaster/frontend/src/components/component-parts/users/Users.jsx
+++ TicketBlaster/frontend/src/components/component-parts/users/Users.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import style from "./users.module.css";
+
 export const Users = () => {
   const [users, setUsers] = useState([]);
   const [popUpUser, setPopUpUser] = useState(false);
@@ -90,81 +92,121 @@ export const Users = () => {
   };
 
   return (
-    <div>
+    <div className={style["users"]}>
       {users
         .filter((user) => user.role !== "administrator")
         .map((user) => {
           return (
-            <div key={user._id}>
-              <div>
+            <div key={user._id} className={style["content"]}>
+              <div className={style["first-section"]}>
                 <img
-                  width={"200px"}
-                  height={"150px"}
+                  className={style["user-image"]}
                   src={`http://localhost:10002/images/${user.image}`}
                   alt={user.name}
                 />
                 <div>
-                  <p>{user.fullname}</p>
-                  <p>{user.email}</p>
+                  <p className={style["user-fullname"]}>{user.fullname}</p>
+                  <p className={style["user-email"]}>{user.email}</p>
                 </div>
               </div>
-              <div>
+              <div className={style["second-section"]}>
                 <div>
                   {user.role === "user" && (
-                    <button onClick={() => makeAdmin(user._id)}>
+                    <button
+                      className={style["make-admin"]}
+                      onClick={() => makeAdmin(user._id)}
+                    >
                       Make Admin
                     </button>
                   )}
                 </div>
                 <div>
                   {user.role === "admin" && (
-                    <button onClick={() => makeUser(user._id)}>
+                    <button
+                      className={style["make-user"]}
+                      onClick={() => makeUser(user._id)}
+                    >
                       Make User
                     </button>
                   )}
                 </div>
-                {user.role !== "administrator" && (
-                  <button onClick={() => deleteUser(user._id)}>
-                    Delete User
-                  </button>
-                )}
+                <div>
+                  {user.role !== "administrator" && (
+                    <button
+                      className={style["delete-user"]}
+                      onClick={() => deleteUser(user._id)}
+                    >
+                      Delete User
+                    </button>
+                  )}
+                </div>
               </div>
             </div>
           );
         })}
       {popUpUser && (
-        <div>
+        <div className={style["popup-wrapper"]}>
           <h2>Are you sure?</h2>
           <p>
             You are about to downgrade a user from administrator. Please proceed
             with caution.
           </p>
-          <div>
-            <button onClick={() => setPopUpUser(false)}>Cancel</button>
-            <button onClick={() => upgradeDegrade()}>Downgrade user</button>
+          <div className={style["buttons-wrapper"]}>
+            <button
+              className={style["cancel"]}
+              onClick={() => setPopUpUser(false)}
+            >
+              Cancel
+            </button>
+            <button
+              className={style["downgrade-user"]}
+              onClick={() => upgradeDegrade()}
+            >
+              Downgrade user
+            </button>
           </div>
         </div>
       )}
       {popUpAdmin && (
-        <div>
+        <div className={style["popup-wrapper"]}>
           <h2>Are you sure?</h2>
           <p>
             You are about to make a user administrator of the system. Please
             proceed with caution.
           </p>
-          <div>
-            <button onClick={() => setPopUpAdmin(false)}>Cancel</button>
-            <button onClick={() => upgradeDegrade()}>Make user admin</button>
+          <div className={style["buttons-wrapper"]}>
+            <button
+              className={style["cancel"]}
+              onClick={() => setPopUpAdmin(false)}
+            >
+              Cancel
+            </button>
+            <button
+              className={style["make-user-admin"]}
+              onClick={() => upgradeDegrade()}
+            >
+              Make user admin
+            </button>
           </div>
         </div>
       )}
       {popUpDelete && (
-        <div>
+        <div className={style["popup-wrapper"]}>
           <h2>Are you sure?</h2>
           <p>You are about to delete a user. Please proceed with caution.</p>
-          <div>
-            <button onClick={() => setPopUpDelete(false)}>Cancel</button>
-            <button onClick={() => removeUser()}>Delete User</button>
+          <div className={style["buttons-wrapper"]}>
+            <button
+              className={style["cancel"]}
+              onClick={() => setPopUpDelete(false)}
+            >
+              Cancel
+            </button>
+            <button
+              className={style["remove-user"]}
+              onClick={() => removeUser()}
+            >
+              Delete User
+            </button>
           </div>
         </div>
       )}
